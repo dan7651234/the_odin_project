338487b4cf536d3b216fb5cbb769cacfd0727da6
Dan Bilunas <Dan7651234@gmail.com>
2019-03-18 19:23:43 -0700

    Adding recent RSpec changes

---
+++ Ruby/ConnectFour/spec/ConnectFourPlayer_spec.rb
@@ -0,0 +1,11 @@
+require 'ConnectFourPlayer'
+
+describe ConnectFourPlayer do
+  let()
+
+  describe '#get_piece' do
+    it 'returns the character used for the player piece'
+    expect
+  end
+
+end
